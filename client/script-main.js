53a33555a6ed66ad3e4d477f66ad7a02e7a4e75e
LouisPages <louis.pagesmarchais@gmail.com>
2026-01-18 00:03:22 +0100

    board init

---
+++ client/script-main.js
@@ -0,0 +1,45 @@
+let curBoard = [
+    ['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'],
+    ['♟', '♟', '♟', '♟', '♟', '♟', '♟', '♟'],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['♟', '♟', '♟', '♟', '♟', '♟', '♟', '♟'],
+    ['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜']
+];
+
+
+
+//create the board with empty cases
+function initBoard() {
+    squares = "";
+    colorSquare = {0: "bright", 1: "dark"};
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 8; j+=2) {
+            squares += "<div class='square square-" + colorSquare[(i+j) % 2] + "' id='square" + String(i) + String(j) + "'></div>\n";
+            squares += "<div class='square square-" + colorSquare[(i+1+j) % 2] + "' id='square" + String(i) + String(j+1) + "'></div>\n";
+        }
+    }
+    chessBoard = document.getElementById("chess-board");
+    chessBoard.innerHTML = squares;
+    initPieces();
+}
+
+//add pieces on the board
+function initPieces() {
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 8; j++) {
+            divPiece = document.getElementById("square" + String(i) + String(j));
+            if (i === 6 || i === 7) {
+                divPiece.innerHTML = "<span class='prevent-select' style='color: white;'>" + curBoard[i][j] + "</span>";
+            }
+            else {
+                divPiece.innerHTML = "<span class='prevent-select'>" + curBoard[i][j] + "</span>";
+            }
+            if (curBoard[i][j] != '') divPiece.classList.add("pointer");
+        }
+    }
+}
+
+window.onload = initBoard();
